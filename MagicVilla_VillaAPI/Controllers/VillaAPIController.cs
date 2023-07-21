9bd4f3c50c11feaf156cacf7e0b6cdc4a95c9fdf
Jeff Schuman <jschuman@solutionstreet.com>
2023-07-21 14:06:26 -0400

    added DTO; added getVilla API

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,22 +1,27 @@
 ﻿using System;
+using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
-    [Route("api/VillaAPI")]
-	[ApiController]
-	public class VillaAPIController : ControllerBase
-	{
-		[HttpGet]
-		public IEnumerable<Villa> GetVillas()
-		{
-			return new List<Villa>
-			{
-				new Villa { Id = 1, Name = "Pool View" },
-				new Villa { Id = 2, Name = "Beach View" }
-			};
-		}
-	}
+    //could do [Route("api/[controller]")] to auto-name, often not recommeded
+    [Route("api/villaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
+        }
+    }
 }
 
