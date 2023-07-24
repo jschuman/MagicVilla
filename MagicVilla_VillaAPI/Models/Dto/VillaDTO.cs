e57016262ab1659312e6f90a18dd34ff1f8e077d
Jeff Schuman <jschuman@solutionstreet.com>
2023-07-24 10:11:59 -0400

    Added Update with Put

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -10,6 +10,9 @@ namespace MagicVilla_VillaAPI.Models.Dto
 		[Required]
 		[MaxLength(30)]
 		public string Name { get; set; }
+		public int Occupancy { get; set; }
+		public int Sqft { get; set; }
+
 	}
 }
 
