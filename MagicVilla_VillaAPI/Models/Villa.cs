b76bbdab768513b02cdb3ceb2070f66a09647c97
Jeff Schuman <jschuman@solutionstreet.com>
2023-08-04 14:33:29 -0400

    Starting with Entity Framework

--- MagicVilla_VillaAPI/Models/Villa.cs
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -1,11 +1,19 @@
-﻿using System;
-namespace MagicVilla_VillaAPI.Models
+﻿namespace MagicVilla_VillaAPI.Models
 {
 	public class Villa
 	{
-		public int Id { get; set; }
+		[key]
+        public int Id { get; set; }
+
 		public string Name { get; set; }
-		public DateTime CreatedDate { get; set; }
-	}
+		public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
 }
 
